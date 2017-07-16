2e75f06d81c198c14795efef8b0f92960d94fdb8
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-07-16 13:10:12 -0500

    added basic logic and took away sound for now. most of it is working

--- calc.js
+++ calc.js
@@ -1,30 +1,187 @@
+'use strict';
 
-let listen = document.querySelector('.calcBody');
-let typeLine = document.querySelector('#typeLine');
-let holder = [];
-let meow = document.querySelector('.calcBody');
-let answerBox = document.querySelector('#typeLine');
+const clear = document.querySelector('#clear');
+const equals = document.querySelector('#equals');
+const typeLine = document.querySelector('#typeLine');
+const buttons = document.querySelectorAll('div.button2');
+let lhs = '';
+let rhs = '';
+let operator = '';
 
+const eventHandler = function(event) {
+  const entry = event.target.textContent;
 
+  if (!operator) {
+    lhs += entry;
+  } else if (!operator && is_operator(entry)) {
+    // Is operator
+    operator = entry;
+    lhs = parseInt(lhs);
+  } else if(is_operator(entry)) {
+    lhs = operate(operator, lhs, rhs)
+    rhs = '';
+    operator = entry;
+  } else {
+    rhs += entry;
+  }
 
-//turns strings into numbers // stores in answerBox
-listen.addEventListener('click', function(evt) {
-  let numberCrunch = parseInt(evt.target.textContent);
-  console.log(typeof numberCrunch,  evt.target.textContent);
-  answerBox += numberCrunch;
-  console.log(answerBox);
+  typeLine.textContent = [ lhs, operator, rhs ].join(' ');
+};
+
+const isOperator = function(entry) {
+  return entry === '+' || entry === '-' || entry === 'X' || entry === '/';
+};
+
+const operate = function(operator, lhs, rhs) {
+  lhs = parseInt(lhs);
+  rhs = parseInt(rhs);
+  if (operator === '+') {
+    return lhs + rhs;
+  } else if (operator === '-') {
+    return lhs - rhs;
+  } else if (operator === 'X') {
+    return lhs * rhs;
+  } else if (operator === '/') {
+    return lhs / rhs;
+  } else {
+    throw new Error('YOU FUCKED UP!!!')
+  }
+};
+
+clear.addEventListener('click', function(event) {
+  lhs = '';
+  rhs = '';
+  operator = '';
+  typeLine.textContent = '';
 });
+
+equals.addEventListener('click', eventHandler);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', eventHandler);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let listen = document.querySelector('.calcBody');
+// let typeLine = document.querySelector('#typeLine');
+// let holder = [];
+// let answerBox = document.querySelector('#typeLine');
+// let meow = document.querySelector('.calcBody');
+// let plus = document.querySelector('#plus')
+// // let answerBox = document.querySelector('#typeLine');
+//
+// let symbols = document.querySelector('.lightgreen');
+//
+//
+// //loops over divs in calcbody to assign event listeners.
+// // function bodyLoop () {
+// // for (let i = 0; i < calcBody.length; i++) {
+// //
+// // }
+// //
+// // }
+// //
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+// //turns strings into numbers // stores in holder
+// listen.addEventListener('click', function(evt) {
+//   console.log("hi", typeof evt.target.textContent);
+//   let numberCrunch = parseInt(evt.target.textContent);
+//   holder.push(numberCrunch);
+//   console.log(holder);
+// });
+
+//function that loops through holder array and posts it to the typeLine.
+// plus.addEventListener('click', function() {
+//   function looper (loop) {
+//   for (let i = 0; i < holder.length; i++) {
+//     typeLine.textContent = result;
+//   }
+//   }
+// });
+
+// answerBox.innerHTML = numberCrunch;
+
+//turns NaN symbols into math things. // stores in answerBox
+// symbols.addEventListener('click', function(evt) {
+//   // let symbolCrunch = parseInt(evt.target.textContent);
+//   let symbolCrunch = evt.target.innerHTML;
+//   console.log(typeof evt.target.innerHTML);
+//   // console.log(typeof numberCrunch,  evt.target.textContent);
+//   if (symbolCrunch === "X") {
+//       answerBox *= symbolCrunch;
+//   } else if {
+//     (symbolCrunch === "/") {
+//       answerBox /= symbolCrunch;
+//     }
+//   }
+//   console.log(answerBox);
+// });
+//
+
+
+
 
 
 //Sound
-meow.addEventListener('click', function(evt) {
-  const audio = new Audio('./Sad-cat.mp3')
-  audio.play();
-});
+// meow.addEventListener('click', function(evt) {
+//   const audio = new Audio('./Sad-cat.mp3')
+//   audio.play();
+// });
 
 
 
-//listener on whole calc :)
+//listener on whole calc
 // able to click and return a value on each number on console.
 //return that value to the screen on typeLine.
 //pushing
@@ -32,3 +189,4 @@ meow.addEventListener('click', function(evt) {
 //able to DO the math and return the answer of the math.
 //clear math from screen.
 // evt.target.innerHTML
+//http://www.baconsizzling.com/
